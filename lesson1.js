15cd3ab413636eb97928fb17f00f401e75232651
Spintopple <42618637+Spintopple@users.noreply.github.com>
2019-01-16 14:03:37 -0500

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,72 @@
+// Let me tell you about that mister man
+
+// Ok now let's begin.
+/*
+    Multiple line comments?!
+    This is complete madness!!
+
+    Never in my life have I heard of such a thing!
+ */
+
+
+ //explaining javascript numbers
+
+
+ var myAgeInJanuary;
+
+ myAgeInJanuary = 20;
+ myAgeInJanuary = myAgeInJanuary + 10;
+
+ myAgeInJanuary += 10;
+ myAgeInJanuary *= 2;
+ myAgeInJanuary /= 4;
+
+var MyNumber = 1;
+MyNumber++;
+MyNumber--;
+
+var newValue = MyNumber + myAgeInJanuary;
+
+// HTML
+
+<table border="1">
+        <body>
+            <thead>
+                <tr>
+                    <tb scope="col" colspan="2">Last Name</tb>
+                    <tb scope="col" colspan="2">First Name</tb>
+                    <tb scope="col">Present</tb>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Adkins</td>
+                    <td>Jared</td>
+                    <td>
+                        <label>
+                                <input type="radio" name="present"></input>
+                        </label>
+                    </td>
+                </tr>
+            </tbody>
+        </body>
+</table>
+
+// More Numbers
+
+var Meage = 4.65;
+Meage = "Bill";
+Meage = 'Bill';
+
+Meage = 4.650324;
+
+
+var BillAge = '48';
+// BillAge = 46 does not work either
+var TedAge = 48;
+
+if (BillAge == TedAge) {
+    // does some cool stuff!
+}
+
+var BillAgeNumber = parsInt(BillAge,10);
